7a4d039d21f33ea34b73753f58269c5a13b619a4
Parthasarathy96 <101387183+Parthasarathy96@users.noreply.github.com>
2022-03-21 10:38:30 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 
-const URL = "https://eth-ropsten.alchemyapi.io/v2/Btr94MtiAJQl0xhB6ktaQ_S75TG1ENy1";
-const private_key = "398a44a32ff96418595b79ac234339e059621bd01275a7a6751dbb15bc89a53d"
+const URL = "https://eth-rinkeby.alchemyapi.io/v2/Btr94MtiAJQl0xhB6ktaQ_S75TG1ENy1";
+const private_key = "PRIVATE_KEY"
 
 
 module.exports = {
   networks: {
-    ropsten: {
+    rinkeby: {
       url: URL,
       accounts: [`0x${private_key}`]
     }
